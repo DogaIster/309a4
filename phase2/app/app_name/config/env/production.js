b8ccd13b1c8bb3774c937a0600575ee79fcc36ef
slideon <adamkasztenny@gmail.com>
2016-03-04 18:53:50 -0500

    Add our MLab db to the config

--- phase2/app/app_name/config/env/production.js
+++ phase2/app/app_name/config/env/production.js
@@ -1,5 +1,7 @@
 'use strict';
 
+process.env.MONGOLAB_URI = 'mongodb://app:app@ds019668.mlab.com:19668/309a4';
+
 module.exports = {
   secure: {
     ssl: true,
@@ -27,7 +29,7 @@ module.exports = {
         directoryPath: process.env.LOG_DIR_PATH || process.cwd(),
         fileName: process.env.LOG_FILE || 'access.log',
         rotatingLogs: { // for more info on rotating logs - https://github.com/holidayextras/file-stream-rotator#usage
-          active: process.env.LOG_ROTATING_ACTIVE === 'true' ? true : false, // activate to use rotating logs 
+          active: process.env.LOG_ROTATING_ACTIVE === 'true' ? true : false, // activate to use rotating logs
           fileName: process.env.LOG_ROTATING_FILE || 'access-%DATE%.log', // if rotating logs are active, this fileName setting will be used
           frequency: process.env.LOG_ROTATING_FREQUENCY || 'daily',
           verbose: process.env.LOG_ROTATING_VERBOSE === 'true' ? true : false
