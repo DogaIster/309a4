bace567ba81d482ff23d43bc05855043a1a606e9
slideon <adamkasztenny@gmail.com>
2016-03-08 18:53:18 -0500

    Add users and signup

--- phase1/mockup/scripts/fakeData.js
+++ phase1/mockup/scripts/fakeData.js
@@ -3,16 +3,50 @@ $(document).ready(function(){
     $('#discussionBoard').click(function() {
 	showDiscussionBoard();
     });
+
+    $('#users').click(function() {
+	showUsers();
+    });
+
+    $('#signIn').click(function() {
+	showSignIn();
+    });
+
+    $('#signUp').click(function() {
+	showSignUp();
+    });
  
 });
 
 var clearInfo = function()
 { 
     $('p').remove();
+    $('#messageBoard').hide();	
+    $('#fakeUsers').hide();
+    $('#fakeSignIn').hide();
+    $('#fakeSignUp').hide();
 };
 
 var showDiscussionBoard = function()
 {
     clearInfo();
     $('#messageBoard').show();
+};
+
+var showUsers = function()
+{
+    clearInfo();
+    $('#fakeUsers').show();
+};
+
+var showSignIn = function()
+{
+    clearInfo();
+    $('#fakeSignIn').show();
+};
+
+var showSignUp = function()
+{
+    clearInfo();
+    $('#fakeSignUp').show();
 };
