1218803991d16f4fe3cf50a65c52d54d7a2602c9
slideon <adamkasztenny@gmail.com>
2016-03-10 17:13:06 -0500

    Add our app name to the header and description

--- phase2/app/app_name/config/env/default.js
+++ phase2/app/app_name/config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'MEAN.JS',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'OfficeHR',
+    description: 'System to book office hours with TAs and Professors',
+    keywords: 'office hr officehr hours prof professor ta tas book students',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
