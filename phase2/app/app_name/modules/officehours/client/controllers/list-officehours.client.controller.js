18bad56ce6b19fcb3a526a0689cfa4a71808ef36
slideon <adamkasztenny@gmail.com>
2016-03-11 19:27:38 -0500

    Add prof button feature

--- phase2/app/app_name/modules/officehours/client/controllers/list-officehours.client.controller.js
+++ phase2/app/app_name/modules/officehours/client/controllers/list-officehours.client.controller.js
@@ -5,10 +5,15 @@
     .module('officehours')
     .controller('OfficehoursListController', OfficehoursListController);
 
-  OfficehoursListController.$inject = ['OfficehoursService'];
+  OfficehoursListController.$inject = ['$scope','OfficehoursService'];
 
-  function OfficehoursListController(OfficehoursService) {
+  function OfficehoursListController($scope, OfficehoursService) {
     var vm = this;
+
+    // set the user variable in the scope for the current logged-in user
+    if (!$scope.user) {
+      $scope.user = window.user;
+    }
 
     vm.officehours = OfficehoursService.query();
   }
