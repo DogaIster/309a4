891581abadd2eb4eb009b7973b84a28b5992a4ea
SeanWiz <triclopszx@gmail.com>
2016-03-18 14:51:56 -0400

    Fixed submissions/interactions on individual OH pages

--- phase2/app/app_name/modules/officehours/client/controllers/officehours.client.controller.js
+++ phase2/app/app_name/modules/officehours/client/controllers/officehours.client.controller.js
@@ -22,12 +22,106 @@
     vm.remove = remove;
     vm.save = save;
 
+    vm.saveInterest = saveInterest;
+    vm.saveComment = saveComment;
+
+    var now = new Date();
+
+    var containsUser = function(array, object) {
+      for (var i = 0; i < array.length; i++) {
+        if (array[i].displayName === object.displayName) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    // other helper function specifically for students
+    var containsCreatedUser = function(array, object) {
+      for (var i = 0; i < array.length; i++) {
+        if (array[i] === object._id) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    // Save Officehour
+    function saveInterest(officehour) {
+      // TODO: move create/update logic to service
+      if ($scope.user && $scope.user.typeOfUser === 'professor') {
+        officehour.professor = $scope.user;
+        officehour.professorName = $scope.user.firstName + ' ' + $scope.user.lastName;
+      }
+
+      else if ($scope.user && $scope.user.typeOfUser === 'ta') {
+        if (!containsUser(officehour.tas, $scope.user)) {
+          officehour.tas.push($scope.user);
+        }
+      }
+
+      else {
+        if (!containsUser(officehour.students, $scope.user)) {
+          officehour.students.push($scope.user);
+        }
+      }
+
+      if (officehour._id) {
+        officehour.$update(successCallback, errorCallback);
+      } else {
+        officehour.$save(successCallback, errorCallback);
+      }
+
+      function successCallback(res) {
+        //$state.go('officehours.list', {
+        //  officehourId: res._id
+        //});
+      }
+
+      function errorCallback(res) {
+        vm.error = res.data.message;
+      }
+    }
+
+    //Save comment
+    function saveComment(officehour) {
+
+      officehour.comments.push(
+        { text: officehour.commentSubmission,
+          time: new Date(),
+          _id: $scope.user._id,
+          profileImageURL: $scope.user.profileImageURL,
+          displayName: $scope.user.displayName
+        }
+      );
+
+      officehour.commentSubmission = undefined;
+
+      if (officehour._id) {
+        officehour.$update(successCallback, errorCallback);
+      } else {
+        officehour.$save(successCallback, errorCallback);
+      }
+
+      function successCallback(res) {
+        //$state.go('officehours.list', {
+        //  officehourId: res._id
+        //});
+      }
+
+      function errorCallback(res) {
+        vm.error = res.data.message;
+      }
+    }
+
     // Remove existing Officehour
     function remove() {
       if (confirm('Are you sure you want to delete?')) {
         vm.officehour.$remove($state.go('officehours.list'));
       }
     }
+
+
 
     // Save Officehour
     function save(isValid) {
